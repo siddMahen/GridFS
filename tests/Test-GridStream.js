f452593b45159c16f260c9b33035045d45d609ef
Siddharth Mahendraker <siddharth_mahen@me.com>
2011-07-18 22:22:13 +0300

    Lots of GridStream improvements, and minor GridFS doc updates.

--- tests/Test-GridStream.js
+++ tests/Test-GridStream.js
@@ -3,27 +3,36 @@ var GridStream = require('../lib/GridStream');
 var GridFS = require('../lib/GridFS');
 
 
-var FStream = new GridStream('test','test','w',function(err, gs){
-	if(err) console.log(err);
-	gs.write('Hey ;)');
+var stream = new GridStream('test','someTest','r',{ 'chunk_size' : 3 }, function(err, gs){
 
-	gs.close();
+	if(err) console.log(err);
+	
+	console.log('Beginning read.');
+	var Data = "";
+
+	gs.on('data', function(data){
+		Data = Data.concat(data.toString('utf-8',0,data.length));
+	});
+	
+	gs.on('end', function(){
+		console.log('Data: '+Data);
+		console.log('Read succeded.');
+	});
+	
+	gs.on('close', function(){
+		console.log('Read closed.');		
+	});
 });
 
-var stream = new GridStream('test','test','r',{ 'chunk_size' : 2 }, function(err, gs){
 
+var FStream = new GridStream('test','test','w',function(err, gs){
 	if(err) console.log(err);
-
-	gs.on('data', function(err, data){
-		if(err) console.log(err);
-		console.log(data);
-		gs.close();
-
-	});
+	console.log('Beginning write.');
+	gs.write('This test should work... ');
+	console.log('Write succeded.');
+	gs.end('Ending...');
 	
-	gs.on('complete', function(){
-		
+	gs.on('close',function(){
+		console.log('Write closed.');
 	});
-	
-	gs.read();
 });
